1149a8f80df2ac2d1eb2c272333f40e514ad9c15
as4235 <as4235@njit.edu>
2020-03-19 00:18:41 -0400

    FEATURE: Added calculator module

---
+++ calc.py
@@ -0,0 +1,45 @@
+class calc:
+
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        return a / b
+
+    @staticmethod
+    def sq(a):
+        return a ** 2
+
+    @staticmethod
+    def rt( a):
+        return a ** 0.5
+
+if __name__ == "__main__":
+        c = calc()
+        print("1.Add 2. Subtract 3. Multiply 4. Divide 5. Square 6. Square Root")
+        i = int(input())
+        if i == 1:
+            print(c.add(10, 20))
+        elif i == 2:
+            print(c.sub(10, 20))
+        elif i == 3:
+            print(c.mul(10, 20))
+        elif i == 4:
+            print(c.div(10, 20))
+        elif i == 5:
+            print(c.sq(10))
+        elif i == 6:
+            print(c.rt(100))
+        else:
+            print("error")
