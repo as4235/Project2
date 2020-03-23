3cb1e0468c57fdaaac45044fed21e2d13d58e990
as4235 <as4235@njit.edu>
2020-03-23 15:05:02 -0400

    FIX: Fixed assertion error

--- test_stat.py
+++ test_stat.py
@@ -9,7 +9,7 @@ class test_stat(unittest.TestCase):
 
     def test_pmean(self):
         from pmean import pmean
-        assert pmean(data) == 805.1
+        assert pmean(data) == 462.0285714285714
 
     def test_pmean_fail(self):
         from pmean import pmean
@@ -17,7 +17,7 @@ class test_stat(unittest.TestCase):
 
     def test_median(self):
         from median import median
-        assert median(data) == 251.0
+        assert median(data) == 235.5
 
     def test_median_fail(self):
         from median import median
@@ -25,7 +25,7 @@ class test_stat(unittest.TestCase):
 
     def test_mode(self):
         from mode import mode
-        assert mode(data) == '123'
+        assert mode(data) == '1'
 
     def test_mode_fail(self):
         from mode import mode
@@ -33,7 +33,7 @@ class test_stat(unittest.TestCase):
 
     def test_popstddev(self):
         from popstddev import popstddev
-        assert popstddev(data) == 1631.899534285123
+        assert popstddev(data) == 1347.0801868648168
 
     def test_popstddev_fail(self):
         from popstddev import popstddev
@@ -41,7 +41,7 @@ class test_stat(unittest.TestCase):
 
     def test_variancepopprop(self):
         from variancepopprop import variancepopprop
-        assert variancepopprop(data) == 482.76540000000006
+        assert variancepopprop(data) == 8.381025000000026
 
     def test_variancepopprop_fail(self):
         from variancepopprop import variancepopprop
@@ -49,7 +49,7 @@ class test_stat(unittest.TestCase):
 
     def test_zscore(self):
         from zscore import zscore
-        assert zscore(data) == 1631.8995342851235
+        assert zscore(data) == 1436.6279228667147
 
     def test_zscore_fail(self):
         from zscore import zscore
@@ -57,7 +57,7 @@ class test_stat(unittest.TestCase):
 
     def test_stdscore(self):
         from stdscore import stdscore
-        assert stdscore(data) == 1631.8995342851233
+        assert stdscore(data) == 1384.8464158727334
 
     def test_stdscore_fail(self):
         from stdscore import stdscore
