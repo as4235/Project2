1032788a5086dacfd480862116b076ef0ae9c1bb
Yahyajaz <60370152+Yahyajaz@users.noreply.github.com>
2020-03-23 12:59:36 -0400

    Update population_variance.py

--- population_variance.py
+++ population_variance.py
@@ -1,16 +1,16 @@
 from FileReader import readCSV
 import math
 
-dataSet = readCSV('data.csv')
+data = readCSV('data.csv')
 
-def pop_variance(dataSet):
-    if len(dataSet) == 0:
+def pop_variance(data):
+    if len(data) == 0:
         return 0
     
-    cal_mean = sum(dataSet) / len(dataSet)
+    cal_mean = sum(data) / len(data)
     
-    variance = sum((data - cal_mean) ** 2 for data in dataSet) / len(dataSet)
+    variance = sum((data - cal_mean) ** 2 for data in data) / len(data)
     return variance
 
 #dataSet = [1,2,3,4,5,6,7,8,9,10]
-print (pop_variance(dataSet))
+print (pop_variance(data))
