72fc9c82d13d75f22e5636f9db11f0cc5949e8b8
Yahyajaz <60370152+Yahyajaz@users.noreply.github.com>
2020-03-23 13:02:17 -0400

    Update sample_SD.py

--- sample_SD.py
+++ sample_SD.py
@@ -1,13 +1,13 @@
 from FileReader import readCSV
 import math
 
-dataSet = readCSV('data.csv', nrows = 10)
+data = readCSV('data.csv', nrows = 10)
 
 def sample_standardDeviation(dataSet):
-    mean = sum(dataSet) / len(dataSet)
-    std = math.sqrt(sum([(val - mean)**2 for val in dataSet])/(len(dataSet) - 1))
+    mean = sum(data) / len(data)
+    std = math.sqrt(sum([(val - mean)**2 for val in data])/(len(data) - 1))
 
     return std
 
-dataSet = [1,2,3,4,5,6,7,8,9,10]
-print (sample_standardDeviation(dataSet))
+#dataSet = [1,2,3,4,5,6,7,8,9,10]
+print (sample_standardDeviation(data))
